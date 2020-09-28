f991bfe98baf41e80f66d83c77a9aceb50374d33
Flybor <18780106592@163.com>
2020-09-28 13:08:47 +0800

    全屏手势库

---
+++ Example/Pods/Target Support Files/CIFullScreenPopGesture/CIFullScreenPopGesture-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CIFullScreenPopGesture : NSObject
+@end
+@implementation PodsDummy_CIFullScreenPopGesture
+@end
---
+++ Example/Pods/Target Support Files/Pods-CIFullScreenPopGesture_Tests/Pods-CIFullScreenPopGesture_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CIFullScreenPopGesture_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_CIFullScreenPopGesture_Tests
+@end
